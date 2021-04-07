c2c100d29d08f186b6d42a34f7133926d09d0eba
pix666 <pix666@ya.ru>
2021-04-07 12:35:33 +0400

    swap quotes

--- {{cookiecutter.project_slug}}/tests/app/urls.py
+++ {{cookiecutter.project_slug}}/tests/app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import IndexView
 
 
-app_name = 'app'
+app_name = "app"
 urlpatterns = [
-    path('', IndexView.as_view()),
+    path("", IndexView.as_view()),
 ]
--- {{cookiecutter.project_slug}}/tests/urls.py
+++ {{cookiecutter.project_slug}}/tests/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('app.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("app.urls")),
 ]
